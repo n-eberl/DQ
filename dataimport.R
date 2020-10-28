2d8495dd17ea45e13d6040ecd25f240ebb7c95af
Niclas Eberl <niclas.eberl@icloud.com>
2020-10-28 13:29:48 +0000

    Added DB Connection via RODBC No specific parameter

--- dataimport.R
+++ dataimport.R
@@ -1,1 +1,17 @@
-print("DQ Test")
+library(RODBC) # RODBC Package für die Verbindung zu einem SQL Server
+
+my_server="ServerName"
+my_db="DatenbankName"
+my_username="Username"
+my_pwd="Passwort"
+
+
+db <- odbcDriverConnect(paste0("DRIVER={SQL Server};
+                                 server=",my_server,";
+                                 database=",my_db,";
+                                 uid=",my_username,";
+                                 pwd=",my_pwd))
+
+
+sql="SELECT * FROM schema.TabellenName" 
+df <- sqlQuery(db,sql)
